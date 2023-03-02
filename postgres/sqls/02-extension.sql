7b6fb4129af7837875d328d198dca19b78317916
Charles Duarte <charlesduarte019@gmail.com>
2023-03-02 17:05:57 -0300

    Update 02-extension.sql

--- postgres/sqls/02-extension.sql
+++ postgres/sqls/02-extension.sql
@@ -2,7 +2,7 @@
 
 ---------------------------------------- EXTENSION
 
-CREATE EXTENSION IF NOT EXISTS "pgcrypto";
+CREATE EXTENSION IF NOT EXISTS "pgcrypto"; -- For UUID, function gen_random_uuid()
 CREATE EXTENSION IF NOT EXISTS "unaccent";
 
 ---------------------------------------- FINISH
